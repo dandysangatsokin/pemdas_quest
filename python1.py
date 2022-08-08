8fb28d891e737ba05f90ad4889180d154f9cfdd9
dandysangatsokin <110508886+dandysangatsokin@users.noreply.github.com>
2022-08-07 18:51:14 -0700

    Add files via upload

---
+++ python1.py
@@ -0,0 +1,12 @@
+#Belajar output
+
+print("Hello world")
+print("Dandy Yoga Pratama")
+print("hobby saya olahraga taekwondo")
+print("makanan kesukaan saya steak daging")
+print("cita cita saya ingin menjadi tentara")
+print("rumah saya di mekarsari permai")
+print("saya besekolah di smk harapan bangsa")
+print("saya ingin bekerja di batalyon korp baret merah")
+print("saya lahir di blitar jawa timur")
+print("Saya pernah bersekolah di smp mutiara bangsa")
---
+++ python2.py
@@ -0,0 +1,13 @@
+# Belajar output
+
+nama = "dandy" # dandy adalah data , nama adalah variable
+umur = 15 
+alamat = "mekarsari cimanggis kota depok"
+
+print(nama)
+print(umur)
+print(alamat)
+
+print(" nama saya adalah " + nama + " umur saya adalah " + str(umur) + " alamat saya " + alamat)
+
+print("nama saya adalah" + nama")
