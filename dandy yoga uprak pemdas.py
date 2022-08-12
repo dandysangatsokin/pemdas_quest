aed5f94c0b34129c73b85973056f23d3ebc94752
dandysangatsokin <110508886+dandysangatsokin@users.noreply.github.com>
2022-08-11 20:42:30 -0700

    Add files via upload

---
+++ dandy yoga uprak pemdas.py
@@ -0,0 +1,9 @@
+nama_depan = input("masukan nama depan: ")
+nama_belakang = input("masukan nama_belakang: ")
+
+print("nama anda adalah", nama_depan, nama_belakang)
+
+celcius = int(input("masukan suhu celcius: "))
+ream = (4/5 * celcius) + 32
+
+print(celcius, "celcius", ream, "reamur")
